abc4a34fdd49be5890e339115728fe6021db4127
Alex J <Fraise@users.noreply.github.com>
2025-09-06 10:13:31 -0400

    More mesh stuff.

--- cluster/configure.sh
+++ cluster/configure.sh
@@ -6,6 +6,13 @@ kubectl apply -f cluster/k8s-gateway-api.yaml
 kubectl label namespace default istio.io/dataplane-mode=ambient
 istioctl waypoint apply -n default --enroll-namespace
 
+# Also enroll the rest of the network stuff in the mesh
+#kubectl label namespace ingress-nginx istio.io/dataplane-mode=ambient
+#istioctl waypoint apply -n ingress-nginx --enroll-namespace
+#
+#kubectl label namespace kube-system istio.io/dataplane-mode=ambient
+#istioctl waypoint apply -n kube-system --enroll-namespace
+
 #visualisation and metrics
 kubectl apply -f cluster/prometheus.yaml
 kubectl apply -f cluster/kiali.yaml

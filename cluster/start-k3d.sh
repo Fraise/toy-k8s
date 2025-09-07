896fc666bd63f62ae432800c594026d9e61b9182
Alex J <Fraise@users.noreply.github.com>
2025-09-06 22:33:12 -0400

    Fully functional k3d deployment.

--- cluster/start-k3d.sh
+++ cluster/start-k3d.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 k3d registry create registry.localhost --port 5000
-k3d cluster create supercluster --registry-use k3d-registry.localhost:5000
+k3d cluster create supercluster --registry-use k3d-registry.localhost:5000 -p "80:80@loadbalancer"
